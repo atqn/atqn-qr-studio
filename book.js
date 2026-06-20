02f4e244dcb0e2ef64bf38309c6f30a4c26c5816
atqn <76124532+atqn@users.noreply.github.com>
2026-06-20 11:33:30 +0300

    Update book.js

--- book.js
+++ book.js
@@ -33,3 +33,85 @@ if (book) {
         "عدد الأكواد: " +
         book.count;
 }
+
+renderQrList();
+
+function renderQrList() {
+
+    const qrList =
+        document.getElementById(
+            "qrList"
+        );
+
+    if (!qrList) return;
+
+    qrList.innerHTML = `
+
+<div class="book-card">
+
+    <h3>
+        QR 001
+    </h3>
+
+    <div class="book-actions">
+
+        <button
+            class="action-btn edit-btn">
+
+            ✏️ تعديل
+
+        </button>
+
+        <button
+            class="action-btn delete-btn">
+
+            🗑 حذف
+
+        </button>
+
+    </div>
+
+    <button
+        class="book-btn">
+
+        📖 فتح
+
+    </button>
+
+</div>
+
+<div class="book-card">
+
+    <h3>
+        QR 002
+    </h3>
+
+    <div class="book-actions">
+
+        <button
+            class="action-btn edit-btn">
+
+            ✏️ تعديل
+
+        </button>
+
+        <button
+            class="action-btn delete-btn">
+
+            🗑 حذف
+
+        </button>
+
+    </div>
+
+    <button
+        class="book-btn">
+
+        📖 فتح
+
+    </button>
+
+</div>
+
+`;
+}
