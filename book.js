63683b6fbd80df6e6e881f03d975dc8ddf84622f
atqn <76124532+atqn@users.noreply.github.com>
2026-06-20 11:28:00 +0300

    Update book.js

--- book.js
+++ book.js
@@ -1,1 +1,35 @@
+const params =
+    new URLSearchParams(
+        window.location.search
+    );
 
+const bookId =
+    Number(
+        params.get("id")
+    );
+
+const books =
+    JSON.parse(
+        localStorage.getItem(
+            "atqn_books"
+        )
+    ) || [];
+
+const book =
+    books.find(
+        b => b.id === bookId
+    );
+
+if (book) {
+
+    document.getElementById(
+        "bookTitle"
+    ).textContent =
+        book.title;
+
+    document.getElementById(
+        "bookCount"
+    ).textContent =
+        "عدد الأكواد: " +
+        book.count;
+}
