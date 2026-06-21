7dec43e23d951fb1c64c408aef57b0c8a60a3c13
atqn <76124532+atqn@users.noreply.github.com>
2026-06-21 22:00:38 +0300

    Update create.js

--- create.js
+++ create.js
@@ -80,11 +80,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 color: "#ffffff"
             },
 
-            scale: 1.3, // 👈 تكبير QR نفسه بدون تكبير الإطار
+            scale: 1.8, // 👈 تكبير QR نفسه بدون تكبير الإطار
             
             imageOptions: {
                 margin: 6,
-                imageSize: 0.8
+                imageSize: 0.35
             }
         });
 
