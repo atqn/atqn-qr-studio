94553a2bbbf2dd915cc04dae617874b6d81cdb2b
atqn <76124532+atqn@users.noreply.github.com>
2026-06-24 11:41:37 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  collection,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmgkN6Glpa0ly_d4e8heB0TiCmV6ieKbw",
+  authDomain: "atqn-qr1.firebaseapp.com",
+  projectId: "atqn-qr1",
+  storageBucket: "atqn-qr1.firebasestorage.app",
+  messagingSenderId: "867770918097",
+  appId: "1:867770918097:web:419b4dc7fefe9e1c4d51f0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+  db,
+  doc,
+  collection,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  getDoc
+};
