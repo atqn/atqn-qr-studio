6e1a7e1caa20d0ee2c13de46bf331af882e67f93
atqn <76124532+atqn@users.noreply.github.com>
2026-06-24 21:24:55 +0300

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,29 @@
+export function loadHeader() {
+
+  const header = document.createElement("header");
+
+  header.className = "topbar";
+
+  header.innerHTML = `
+    <div class="brand">
+        <img src="assets/atqn-logo.png" alt="ATQN">
+        <h1>ATQN QR Studio</h1>
+    </div>
+
+    <nav>
+        <a href="index.html">الرئيسة</a>
+        <a href="library.html">مكتبة الكتب</a>
+        <a href="create.html">إنشاء QR</a>
+        <a href="#" id="logoutBtn">خروج</a>
+    </nav>
+  `;
+
+  document.body.prepend(header);
+
+  document.getElementById("logoutBtn").onclick = () => {
+    import("./auth.js").then(m => m.logout());
+  };
+}
+
+
 
