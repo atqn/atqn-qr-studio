43a506f322eb298b75cf7a60565bd1cb545388ce
atqn <76124532+atqn@users.noreply.github.com>
2026-06-24 19:08:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,9 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT"
+  apiKey: "AIzaSyBmgkN6Glpa0ly_d4e8heB0TiCmV6ieKbw",
+  authDomain: "atqn-qr1.firebaseapp.com",
+  projectId: "atqn-qr1",
+  storageBucket: "atqn-qr1.firebasestorage.app",
+  messagingSenderId: "867770918097",
+  appId: "1:867770918097:web:419b4dc7fefe9e1c4d51f0",
+  measurementId: "G-X609Z298YZ"
 };
 
 const app = initializeApp(firebaseConfig);
