8d6891dae5f8bb21ca9c4b9e0dc006ec566dde18
atqn <76124532+atqn@users.noreply.github.com>
2026-06-24 16:12:33 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,48 @@
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+/* ======================
+   LOGIN
+====================== */
+export async function login(email, password) {
+
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+/* ======================
+   LOGOUT
+====================== */
+export async function logout() {
+
+  await signOut(auth);
+  window.location.href = "login.html";
+}
+
+/* ======================
+   RESET PASSWORD
+====================== */
+export async function resetPassword(email) {
+
+  return await sendPasswordResetEmail(auth, email);
+}
+
+/* ======================
+   GUARD (PROTECTION)
+====================== */
+export function authGuard(callback) {
+
+  onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+      window.location.href = "login.html";
+      return;
+    }
+
+    callback(user);
+  });
+}
