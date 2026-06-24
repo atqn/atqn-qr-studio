043136965b0439ba696aa58773b896b910b07cb7
atqn <76124532+atqn@users.noreply.github.com>
2026-06-24 13:44:07 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import {
   getFirestore,
   doc,
-  collection,
-  onSnapshot,
   setDoc,
-  updateDoc,
-  getDoc
+  getDoc,
+  onSnapshot,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+/* ======================
+   CONFIG
+====================== */
 const firebaseConfig = {
   apiKey: "AIzaSyBmgkN6Glpa0ly_d4e8heB0TiCmV6ieKbw",
   authDomain: "atqn-qr1.firebaseapp.com",
@@ -18,15 +20,20 @@ const firebaseConfig = {
   appId: "1:867770918097:web:419b4dc7fefe9e1c4d51f0"
 };
 
+/* ======================
+   INIT APP
+====================== */
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+/* ======================
+   CLEAN EXPORTS (NO WINDOW HACKS)
+====================== */
 export {
   db,
   doc,
-  collection,
-  onSnapshot,
   setDoc,
-  updateDoc,
-  getDoc
+  getDoc,
+  onSnapshot,
+  updateDoc
 };
