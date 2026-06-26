d0c3560eafbbc5be980c131a7c3473fde2bb9e57
atqn <76124532+atqn@users.noreply.github.com>
2026-06-26 16:43:28 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,15 @@
+function showToast(message, type = "success") {
+    const toast = document.createElement("div");
+    toast.className = `toast show ${type}`;
+    toast.innerText = message;
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
+}
+
 import { auth } from "./firebase.js";
 
 import {
